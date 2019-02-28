66d828233161b914633d31a77bc0e40f004202a8
DanteManriqueOrellano <relaxurbano@me.com>
2019-02-27 20:30:10 -0500

    asd

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ const server = new ApolloServer({
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
